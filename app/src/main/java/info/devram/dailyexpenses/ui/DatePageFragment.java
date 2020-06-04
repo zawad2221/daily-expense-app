0aafcc5e41debe83525376f389690c269127025f
Rahul Malik <devover30@gmail.com>
2020-06-04 22:36:46 +0530

    tab layout started

---
+++ app/src/main/java/info/devram/dailyexpenses/ui/DatePageFragment.java
@@ -0,0 +1,26 @@
+package info.devram.dailyexpenses.ui;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import info.devram.dailyexpenses.R;
+
+public class DatePageFragment extends Fragment {
+
+    public DatePageFragment() {
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.date_fragment,container,false);
+    }
+}
---
+++ app/src/main/java/info/devram/dailyexpenses/ui/ExpensePageFragment.java
@@ -0,0 +1,25 @@
+package info.devram.dailyexpenses.ui;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import info.devram.dailyexpenses.R;
+
+public class ExpensePageFragment extends Fragment {
+
+    public void ExpensePageFragment() {}
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.date_fragment,container,false);
+    }
+}
